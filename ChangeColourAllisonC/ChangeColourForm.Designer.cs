7415e2ba46642b036269a32ad0b1759f60a456c3
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-04-09 13:02:07 -0400

    names

--- ChangeColourAllisonC/ChangeColourForm.Designer.cs
+++ ChangeColourAllisonC/ChangeColourForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChangeColourAllisonC
 {
-    partial class Form1
+    partial class frmChangeColour
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblChangeColour = new System.Windows.Forms.Label();
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblChangeColour
+            // 
+            this.lblChangeColour.AutoSize = true;
+            this.lblChangeColour.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblChangeColour.Font = new System.Drawing.Font("Kozuka Mincho Pr6N H", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblChangeColour.Location = new System.Drawing.Point(72, 152);
+            this.lblChangeColour.Name = "lblChangeColour";
+            this.lblChangeColour.Size = new System.Drawing.Size(125, 28);
+            this.lblChangeColour.TabIndex = 0;
+            this.lblChangeColour.Text = "Change Colour";
+            this.lblChangeColour.Click += new System.EventHandler(this.lblChangeColour_Click);
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Font = new System.Drawing.Font("Kozuka Mincho Pr6N H", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeColour.Location = new System.Drawing.Point(65, 59);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(143, 38);
+            this.btnChangeColour.TabIndex = 1;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // frmChangeColour
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnChangeColour);
+            this.Controls.Add(this.lblChangeColour);
+            this.Name = "frmChangeColour";
+            this.Text = "Chnage Colour by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblChangeColour;
+        private System.Windows.Forms.Button btnChangeColour;
     }
 }
 
--- ChangeColourAllisonC/ChangeColourForm.cs
+++ ChangeColourAllisonC/ChangeColourForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 9 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Change colour
+ * This program changes the colour of all objects when a button is clicked and changes
+ * the colour of a label when clicked to a different colour than the button
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +18,26 @@ using System.Windows.Forms;
 
 namespace ChangeColourAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmChangeColour : Form
     {
-        public Form1()
+        public frmChangeColour()
         {
             InitializeComponent();
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            //loop through each object and change the colour
+            foreach (Control aControlObject in this.Controls)
+            {
+
+            }
+
+        }
+
+        private void lblChangeColour_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
